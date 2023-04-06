14e37c0ebb8840c0a48d2b4fc6d20729530b984f
Jo Carr <hulaus21@gmail.com>
2023-04-05 23:45:02 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-require('./.gitignore/node_modules/dotenv/lib/main').config()
 const express = require('express')
 const app = express()
 
-app.use('places', require('./controllers/places'))
-
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
-
-app.listen(process.env.PORT)
+app.listen(3000)
