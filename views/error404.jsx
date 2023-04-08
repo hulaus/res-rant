1003c57b0304f24af6e8017fb4f367a328adbe2a
Jo Carr <hulaus21@gmail.com>
2023-04-07 22:31:15 -0700

    added Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -4,8 +4,14 @@ const Def = require('./default')
 function error404() {
     return(
         <Def>
-            <main>
+            <main>    
             <h1>404: PAGE NOT FOUND</h1>
+            <div>
+                <img src="/images/pompic.jpg" alt='Picture of Pom'/>
+                <div>
+                Photo by <a href="https://unsplash.com/@cookiethepom?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Cookie the Pom</a> on <a href="https://unsplash.com/photos/gySMaocSdqs?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+                </div>
             <p>oops, sorry, we can't find this page ! :( </p>
             </main>
         </Def>
--- views/home.jsx
+++ views/home.jsx
@@ -7,8 +7,9 @@ function home() {
             <main>
                 <h1>HOME</h1>
                 <div>
-
+                <img src="/images/healthyfood.jpg" alt="Healthy Food Bowl"/>
                 <div>
+                Photo by <a href="https://unsplash.com/@lvnatikk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Lily Banse</a> on <a href="https://unsplash.com/photos/-YHSwy6uqvk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                 </div>
                 </div>
                 <a href='/places'>
