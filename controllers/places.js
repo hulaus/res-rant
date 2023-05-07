46249ab136e327572e13130a7561d637b249eeda
Jo Carr <hulaus21@gmail.com>
2023-05-06 18:47:06 -0700

    fixed bug where the pages weren't being posted but it is fixed now

--- controllers/places.js
+++ controllers/places.js
@@ -1,36 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('places/index', { places });
-});
-
-router.post('/', (req, res) => {
-  console.log(req.body)
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
-
-
-router.get('/new', (req, res) => {
-  res.render('places/new');
-});
-
-router.get('/:id', (req, res) => {
-  res.render('places/id');
-});
-
-
 const places = [
   {
     name: 'H-Thai-ML',
@@ -48,6 +18,22 @@ const places = [
   }
 ];
 
+router.get('/', (req, res) => {
+  res.render('places/index', { places });
+});
 
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
+router.get('/new', (req, res) => {
+  res.render('places/new');
+});
+
+router.get('/:id', (req, res) => {
+  res.render('places/id');
+});
 
 module.exports = router;
